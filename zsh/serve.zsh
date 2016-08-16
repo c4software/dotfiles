c5dbebf21da6a02bb1e9a43447c9dc1f8683e2c1
Brosseau Valentin <c4software@gmail.com>
2016-08-16 13:34:31 +0200

    Search for an available port before serve :)

--- zsh/serve.zsh
+++ zsh/serve.zsh
@@ -1,5 +1,13 @@
 function serve() {
-    local port="${1:-8000}"
-    open "http://localhost:${port}/"
-    python -m SimpleHTTPServer "$port"
+  echo "Scan open port 8888…8899"
+  for (( PORTNUMBER = 8888; PORTNUMBER < 8899; PORTNUMBER++ )); do
+    # Test si port dispo
+    PDISPO=`netstat -an | grep $PORTNUMBER | grep -i listen | wc -l | awk '{print $1}'`
+    if [ $PDISPO -eq 0 ]
+      then
+        echo "Open port found : $PORTNUMBER"
+        python3 -m http.server $PORTNUMBER
+        break
+    fi
+  done
 }
