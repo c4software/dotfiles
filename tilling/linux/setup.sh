f8ba03d522901ddea1e8a64e9e0061f5047ad1b8
Valentin Brosseau <c4software@gmail.com>
2017-04-17 21:53:42 +0200

    Ajout script pour installation

--- tilling/linux/setup.sh
+++ tilling/linux/setup.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-mkdir -p ~/.config/{polybar,bspwm,sxhkd}
+mkdir -p ~/.config/
+cp -r {bspwm,polybar,sxhkd} ~/.config/
+
+yaourt -Sy bspwm j4-dmenu-desktop feh sxhkd xtitle ttf-font-awesome ttf-roboto polybar urxvt
