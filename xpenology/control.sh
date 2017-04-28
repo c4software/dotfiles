8b9cee91aea2243282d8dc3b39b83077e29b49b5
Valentin Brosseau <vbrosseau@afone.com>
2017-04-28 16:19:52 +0200

    Ajout test si Vbox est présent

--- xpenology/control.sh
+++ xpenology/control.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+type VBoxManage >/dev/null 2>&1 || { echo >&2 "VirtualBox is required. Aborting."; exit 1; }
+
 case "$C" in
 "start")
-    VBoxManage startvm "xpenology" --type headless
+    VBoxManage startvm xpenology --type headless
     ;;
 "stop")
     VBoxManage controlvm xpenology poweroff
