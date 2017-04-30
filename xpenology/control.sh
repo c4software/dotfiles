ef10c02c9151e4c3e89961684e43a37d5dee9b61
Valentin Brosseau <c4software@gmail.com>
2017-04-30 09:57:41 +0200

    Ajout gestion de l’ajout de port « temporaire » dans le script de control

--- xpenology/control.sh
+++ xpenology/control.sh
@@ -15,6 +15,21 @@ case "$1" in
 "pause")
     VBoxManage controlvm xpenology savestate
     ;;
+"tmpnat")
+    if [ -z $2 ];then
+       echo "Usage: $0 $1 AppName PortOnHost [PortOnVM]"
+       exit
+    fi
+    if [ -z $3 ];then
+       echo "Usage: $0 $1 $2 PortOnHost [PortOnVM]"
+       exit
+    fi
+    vmport=$4
+    if [ -z $vmport ];then
+      vmport=$3
+    fi
+    VBoxManage controlvm "xpenology" natpf1 "$2,tcp,,$3,,$vmport"
+    ;;
 "console")
    type socat >/dev/null 2>&1 || { echo >&2 "Socat is required. Aborting."; exit 1; }
    socat - UNIX-CONNECT:xpenology-serial
