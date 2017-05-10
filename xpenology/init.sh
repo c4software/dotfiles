282f3dfc676b83f6070a4bb3c51e0bdd39547b40
Valentin Brosseau <c4software@gmail.com>
2017-05-10 22:12:18 +0200

    Passage sur un nom variable de machine pour le script d’init

--- xpenology/init.sh
+++ xpenology/init.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
+VMNAME=$1
+if [ -z $VMNAME ];then
+  VMNAME="xpenology"
+fi
+
 # Create VM
-VBoxManage createvm --name xpenology --register
+VBoxManage createvm --name $VMNAME --register
 
 # Basic config
-VBoxManage modifyvm xpenology --memory 1024 --ostype "debian_64" --nictype1 82540EM --audio none --usb off --nic1 nat --nataliasmode1 proxyonly --uart1 0x3F8 4 --uartmode1 server xpenology-serial
+VBoxManage modifyvm $VMNAME --memory 1024 --ostype "debian_64" --nictype1 82540EM --audio none --usb off --nic1 nat --nataliasmode1 proxyonly --uart1 0x3F8 4 --uartmode1 server $VMNAME-serial
 
 # Add IDE « Boot Drive »
-VBoxManage storagectl xpenology --name "IDE Controller" --add ide
-VBoxManage storageattach xpenology --storagectl "IDE Controller" --port 0 --device 0 --type hdd --medium synoboot.vmdk
+VBoxManage storagectl $VMNAME --name "IDE Controller" --add ide
+VBoxManage storageattach $VMNAME --storagectl "IDE Controller" --port 0 --device 0 --type hdd --medium synoboot.vmdk
 
 # Add SCSI Drive
-VBoxManage storagectl xpenology --name "SCSI Controller" --add scsi
+VBoxManage storagectl $VMNAME --name "SCSI Controller" --add scsi
 # Create a 20Gb SCSI drive
 vboxmanage createmedium disk --filename hd1.vdi --size 20000
-VBoxManage storageattach xpenology --storagectl "SCSI Controller" --port 0 --device 0 --type hdd --medium hd1.vdi
+VBoxManage storageattach $VMNAME --storagectl "SCSI Controller" --port 0 --device 0 --type hdd --medium hd1.vdi
 
 # Port Forward
-VBoxManage modifyvm xpenology --natpf1 "app,tcp,,5000,,5000"
+VBoxManage modifyvm $VMNAME --natpf1 "app,tcp,,5000,,5000"
 
 # Start
-VBoxManage startvm xpenology --type headless
+VBoxManage startvm $VMNAME --type headless
 
 echo "In 1 or 2 minutes your VM should be accessible : http://localhost:5000"
 
