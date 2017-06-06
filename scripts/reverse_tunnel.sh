fb77d9c3c0adcbb8c3c323d90d3be9b8edeff0ac
Valentin Brosseau <c4software@gmail.com>
2017-06-06 22:35:08 +0200

    Réécriture du script pour permetre le passage de plusieurs port, utilisation de flags

--- scripts/reverse_tunnel.sh
+++ scripts/reverse_tunnel.sh
@@ -1,29 +1,53 @@
 #!/bin/bash
 
+function show_help() {
+    echo "Usage:"
+    echo "  $0 -r REMOTEHOST -p LOCALPORT[:REMOTEPORT]"
+    echo "     -p: Can be multiple"
+    exit
+}
+
 if [ $# -eq 0 ]; then
-  echo "Usage: $0 host port [remote-port]"
-  exit
+	show_help;
 fi
 
+# Get all p parameters
+while getopts "p:r:" opt; do
+    case $opt in
+        r) HOST=$OPTARG;;
+        p) PORTS+=("$OPTARG");;
+    esac
+done
+
+# Create SSH command
+SSH_REMOTE_ARGS=""
+for val in "${PORTS[@]}"; do
+    split_port=(${val//:/ })
+    if [ ${#split_port[@]} -eq 1 ]; then
+        REMOTEPORT=${split_port[0]}
+        LOCALPORT=${split_port[0]}
+    else
+        REMOTEPORT=${split_port[1]}
+        LOCALPORT=${split_port[0]}
+    fi
+    SSH_REMOTE_ARGS+=" -R 0.0.0.0:$REMOTEPORT:0.0.0.0:$LOCALPORT"
+done
+
+# Get Remote Server
 if [ -z "$1" ]; then
 	echo "Remote server is mandatory"
 	exit
 fi
 
-if [ -z "$2" ]; then
-	echo "Port is mandatory"
-	exit
+# If no ports, then show help, and stop
+if [ -z "$SSH_REMOTE_ARGS" ]; then
+	show_help
 fi
 
-if [ -z "$3" ]; then
-	REMOTEPORT=$2
-else
-	REMOTEPORT=$3
-fi
 
-while : 
-do	
-echo "$(date "+%Y-%m-%d %H:%M:%S") : $REMOTEPORT:localhost:$2 => $1"
-ssh -NR 0.0.0.0:$REMOTEPORT:0.0.0.0:$2 $1
-sleep 10
+while :
+do
+	echo "$(date "+%Y-%m-%d %H:%M:%S") : $REMOTEPORT:localhost:$2 => $1"
+	echo "ssh -N $SSH_REMOTE_ARGS $HOST"
+	sleep 10
 done
