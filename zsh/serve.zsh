c80dcc96d243f8789f8057e52ab02f260b34c656
Valentin Brosseau <valentin@brosseau.ovh>
2018-01-18 22:41:46 +0100

    Migration vers ss plutôt que netstat car plus dispo...

--- zsh/serve.zsh
+++ zsh/serve.zsh
@@ -2,7 +2,7 @@ function serve() {
   echo "Scan open port 8888…8899"
   for (( PORTNUMBER = 8888; PORTNUMBER < 8899; PORTNUMBER++ )); do
     # Test si port dispo
-    PDISPO=`netstat -an | grep $PORTNUMBER | grep -i listen | wc -l | awk '{print $1}'`
+    PDISPO=`ss -an | grep $PORTNUMBER | grep -i listen | wc -l | awk '{print $1}'`
     if [ $PDISPO -eq 0 ]
       then
         echo "Open port found : $PORTNUMBER"
