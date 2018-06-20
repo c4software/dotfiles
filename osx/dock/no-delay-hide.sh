03f6148f283b096603c2f2cdaf20a8f8ac3e10bd
Valentin Brosseau <c4software@gmail.com>
2018-06-20 16:27:37 +0200

    Ajout script pour masquer et afficher le dock plus rapidement

---
+++ osx/dock/no-delay-hide.sh
@@ -0,0 +1,4 @@
+defaults write com.apple.dock autohide -bool true 
+defaults write com.apple.dock autohide-delay -int 0
+defaults write com.apple.dock autohide-time-modifier -float 0.2
+killall Dock
