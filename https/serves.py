66fc47be5723e59d28603f75efe3f50628f467d2
Valentin Brosseau <c4software@gmail.com>
2019-10-17 09:16:17 +0200

    Ajout d'un script pour créer un server HTTPS de dev

---
+++ https/serves.py
@@ -0,0 +1,18 @@
+# taken from http://www.piware.de/2011/01/creating-an-https-server-in-python/
+# generate server.xml with the following command:
+#    openssl req -new -x509 -keyout server.pem -out server.pem -days 365 -nodes
+# run as follows:
+#    python simple-https-server.py
+# then in your browser, visit:
+#    https://localhost:4443
+
+import BaseHTTPServer, SimpleHTTPServer
+import ssl
+import os
+
+script_path = os.path.dirname(os.path.abspath(__file__))
+
+httpd = BaseHTTPServer.HTTPServer(('localhost', 4443), SimpleHTTPServer.SimpleHTTPRequestHandler)
+httpd.socket = ssl.wrap_socket (httpd.socket, certfile=script_path + '/server.pem', server_side=True)
+print ("Server is now listening: https://localhost:4443")
+httpd.serve_forever()
