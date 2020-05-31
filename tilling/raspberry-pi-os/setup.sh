48b68026b0056ed56cf2a3e7e90bcaf5cf8d14a7
Valentin Brosseau <c4software@gmail.com>
2020-05-31 22:49:32 +0200

    Add Terminus font

--- tilling/raspberry-pi-os/setup.sh
+++ tilling/raspberry-pi-os/setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-sudo apt install -y bspwm j4-dmenu-desktop feh sxhkd xtitle rxvt-unicode lemonbar bc wmctrl
+sudo apt install -y bspwm j4-dmenu-desktop feh sxhkd xtitle rxvt-unicode lemonbar bc wmctrl xfonts-terminus
 mkdir -p ~/.config/{bspwm,sxhkd,lemonbar}
+cp .Xdefaults ~/.Xdefaults
