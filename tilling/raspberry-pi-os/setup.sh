e6178cad8ab798f34344955c82fac7bcc08a0541
Valentin Brosseau <c4software@gmail.com>
2020-06-02 22:14:44 +0200

    Update

--- tilling/raspberry-pi-os/setup.sh
+++ tilling/raspberry-pi-os/setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "# Install packages"
 sudo apt install -y bspwm j4-dmenu-desktop feh sxhkd xtitle rxvt-unicode lemonbar bc wmctrl xfonts-terminus
 mkdir -p ~/.config/{bspwm,sxhkd,lemonbar}
+
+echo "Copy configurations files"
 cp .Xdefaults ~/.Xdefaults
+cp lemonbar/panel.sh ~/.config/lemonbar/panel.sh
