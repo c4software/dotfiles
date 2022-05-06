1131a759324e72298b6f8c88fdc8eb8cb9af839c
Brosseau Valentin <c4software@gmail.com>
2022-05-06 22:32:20 +0200

    Update setup.sh

--- tilling/linux/setup.sh
+++ tilling/linux/setup.sh
@@ -3,6 +3,6 @@
 mkdir -p ~/.config/
 cp -r {bspwm,polybar,sxhkd} ~/.config/
 
-yaourt -Sy bspwm j4-dmenu-desktop feh sxhkd xtitle ttf-font-awesome ttf-roboto polybar urxvt
+yaourt -Sy bspwm j4-dmenu-desktop feh sxhkd xtitle ttf-font-awesome ttf-roboto polybar xtitle scrot i3lock kitty nerd-fonts-sf-mono
 
 echo "exec bspwm" > ~/.xinitrc
