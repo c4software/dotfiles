5ab009417ec599d0a5f5753e8d7eb14a875e72b6
Valentin Brosseau <c4software@gmail.com>
2025-08-22 19:20:10 +0200

    Correction du chemin d'accès au script de configuration et ajout de la source des scripts d'applications et de bureau

--- fedora/setup.sh
+++ fedora/setup.sh
@@ -3,4 +3,14 @@
 clear
 
 source ./install/init.sh
-source ./install/config/bootsrap.sh
+source ./install/config/bootstrap.sh
+
+# Source all script under install/apps
+for script in ./install/apps/*.sh; do
+  source "$script"
+done
+
+# Source all scripts under desktop
+for script in ./install/desktop/*.sh; do
+  source "$script"
+done
