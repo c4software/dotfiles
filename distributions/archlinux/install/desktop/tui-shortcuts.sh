244ee23d7c23370e6ea7a936a77d1eb00645fe6e
Valentin Brosseau <c4software@gmail.com>
2025-08-24 22:03:32 +0200

    Fix script name

--- distributions/archlinux/install/desktop/tui-shortcuts.sh
+++ distributions/archlinux/install/desktop/tui-shortcuts.sh
@@ -1,4 +1,4 @@
 # Create TUI Shortcut for LazyDocker, k9s
 
-webapp-install "Docker" "lazydocker" "tile" "https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/docker.png"
-webapp-install "K9s" "k9s" "tile" "https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/kubernetes-dashboard.png"
+tui-install "Docker" "lazydocker" "tile" "https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/docker.png"
+tui-install "K9s" "k9s" "tile" "https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/kubernetes-dashboard.png"
