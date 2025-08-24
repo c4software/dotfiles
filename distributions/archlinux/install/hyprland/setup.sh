18814865207a80095f61fa33ea361dda6902c6c7
Valentin Brosseau <c4software@gmail.com>
2025-08-24 22:20:08 +0200

    Move specific configuration to Hyprland folder

--- distributions/archlinux/install/hyprland/setup.sh
+++ distributions/archlinux/install/hyprland/setup.sh
@@ -4,3 +4,6 @@ yay -S --noconfirm --needed \
   hyprland hyprshot hyprpicker hyprlock hypridle hyprsunset polkit-gnome hyprland-qtutils \
   walker-bin libqalculate waybar mako swaybg swayosd \
   xdg-desktop-portal-hyprland xdg-desktop-portal-gtk
+
+echo -e "Moving configuration files"
+cp -r ./config/* ~/.config/
