7922a07278a2860d87bc3d42a54bce5e424969fc
Valentin Brosseau <c4software@gmail.com>
2025-08-25 16:43:25 +0200

    Fix

--- distributions/common/install/bootstrap.sh
+++ distributions/common/install/bootstrap.sh
@@ -18,15 +18,19 @@ cp "$SCRIPT_DIR/../bin/"* ~/.local/bin/
 # Install Bash configuration
 cp "$SCRIPT_DIR/../default/bashrc" ~/.bashrc
 
-# Installation du layout de clavier Bépo Dev
-echo "Installing Bépo Dev keyboard layout (Système)..."
-sudo wget -nc https://raw.githubusercontent.com/c4software/bepo_developpeur/master/linux/bepoDev -O /usr/share/X11/xkb/symbols/bepoDev
+# If not MacOS
+if [[ "$OSTYPE" != "darwin"* ]]; then
+  # Installation du layout de clavier Bépo Dev
+  echo "Installing Bépo Dev keyboard layout (Système)..."
+  sudo wget -nc https://raw.githubusercontent.com/c4software/bepo_developpeur/master/linux/bepoDev -O /usr/share/X11/xkb/symbols/bepoDev || true
 
-# Installation bepoDev pour l'utilisateur
-echo "Installing Bépo Dev keyboard layout (Utilisateur)..."
-mkdir -p ~/.config/xkb/symbols ~/.config/xkb/rules
-wget https://raw.githubusercontent.com/c4software/bepo_developpeur/master/linux/bepoDev -O ~/.config/xkb/symbols/bepoDev
-wget https://raw.githubusercontent.com/c4software/bepo_developpeur/master/linux/evdev.lst -O ~/.config/xkb/rules/evdev.lst
-wget https://raw.githubusercontent.com/c4software/bepo_developpeur/master/linux/evdev.xml -O ~/.config/xkb/rules/evdev.xml
-[ -L ~/.config/xkb/rules/base.lst ] || ln -s ~/.config/xkb/rules/evdev.lst ~/.config/xkb/rules/base.lst
-[ -L ~/.config/xkb/rules/base.xml ] || ln -s ~/.config/xkb/rules/evdev.xml ~/.config/xkb/rules/base.xml
+  # Installation bepoDev pour l'utilisateur
+  echo "Installing Bépo Dev keyboard layout (Utilisateur)..."
+  mkdir -p ~/.config/xkb/symbols ~/.config/xkb/rules || true
+  wget https://raw.githubusercontent.com/c4software/bepo_developpeur/master/linux/bepoDev -O ~/.config/xkb/symbols/bepoDev || true
+  wget https://raw.githubusercontent.com/c4software/bepo_developpeur/master/linux/evdev.lst -O ~/.config/xkb/rules/evdev.lst || true
+  wget https://raw.githubusercontent.com/c4software/bepo_developpeur/master/linux/evdev.xml -O ~/.config/xkb/rules/evdev.xml || true
+  [ -L ~/.config/xkb/rules/base.lst ] || ln -s ~/.config/xkb/rules/evdev.lst ~/.config/xkb/rules/base.lst || true
+  [ -L ~/.config/xkb/rules/base.xml ] || ln -s ~/.config/xkb/rules/evdev.xml ~/.config/xkb/rules/base.xml || true
+fi
+fi
