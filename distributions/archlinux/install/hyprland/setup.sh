57fee9b2273021b6348b73769906b7494ca03f97
Valentin Brosseau <c4software@gmail.com>
2025-08-25 15:28:37 +0200

    Get ride of the theme folder

--- distributions/archlinux/install/hyprland/setup.sh
+++ distributions/archlinux/install/hyprland/setup.sh
@@ -3,7 +3,7 @@ echo -e "Installing Hyprland"
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 yay -S --noconfirm --needed \
-  hyprland hyprshot hyprpicker hyprlock hypridle hyprsunset polkit-gnome hyprland-qtutils \
+  hyprland hyprshot hyprpicker hyprlock hypridle polkit-gnome hyprland-qtutils \
   walker-bin libqalculate waybar mako swaybg swayosd \
   xdg-desktop-portal-hyprland xdg-desktop-portal-gtk
 
--- distributions/common/config/nvim/lua/plugins/theme.lua
+++ distributions/common/config/nvim/lua/plugins/theme.lua
@@ -6,3 +6,15 @@ return {
 		},
 	},
 }
+
+/**
+return {
+	"ribru17/bamboo.nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("bamboo").setup({})
+		require("bamboo").load()
+	end,
+}
+**/
