cad99d24d9ce44ea1f16f5b69e78faa7305f820b
Valentin Brosseau <c4software@gmail.com>
2025-08-25 15:34:50 +0200

    Add missing tools and simplify configuration

--- distributions/archlinux/install/hyprland/setup.sh
+++ distributions/archlinux/install/hyprland/setup.sh
@@ -5,7 +5,8 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 yay -S --noconfirm --needed \
   hyprland hyprshot hyprpicker hyprlock hypridle polkit-gnome hyprland-qtutils \
   walker-bin libqalculate waybar mako swaybg swayosd \
-  xdg-desktop-portal-hyprland xdg-desktop-portal-gtk
+  xdg-desktop-portal-hyprland xdg-desktop-portal-gtk \
+  wiremix pamixer blueberry
 
 echo -e "Moving configuration files"
 cp -r "$SCRIPT_DIR/config/*" ~/.config/
