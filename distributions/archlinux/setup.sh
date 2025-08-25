a961a79e721279c0e20541696284d293c356a32b
Valentin Brosseau <c4software@gmail.com>
2025-08-25 16:56:04 +0200

    Fix

--- distributions/archlinux/setup.sh
+++ distributions/archlinux/setup.sh
@@ -6,6 +6,9 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 source "$SCRIPT_DIR/install/init.sh"
 
+# Add local bin to PATH (since we installed binaries there)
+export PATH="$HOME/.local/bin:$PATH"
+
 # Source all script under install/apps
 for script in "$SCRIPT_DIR/install/apps/"*.sh; do
   source "$script"
