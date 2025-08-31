a0d798d752d2fe4ff1c1396da4add132e4e4a81f
Valentin Brosseau <c4software@gmail.com>
2025-08-31 15:43:02 +0200

    Ajout de tldr à la liste des applications terminales à installer

--- distributions/archlinux/install/apps/terminal.sh
+++ distributions/archlinux/install/apps/terminal.sh
@@ -1,6 +1,6 @@
 # Installation des applications terminales
 echo -e "Installing terminal applications"
-sudo pacman -S --noconfirm --needed fzf ripgrep zoxide bat jq xmlstarlet zip unzip curl wget unrar eza ndu gum  bash-completion btop fastfetch zellij
+sudo pacman -S --noconfirm --needed tldr fzf ripgrep zoxide bat jq xmlstarlet zip unzip curl wget unrar eza ndu gum  bash-completion btop fastfetch zellij
 
 # Installation de Starship
 echo -e "Installing Starship (https://starship.rs/)"
